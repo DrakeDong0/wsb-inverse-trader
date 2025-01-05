2a27075dad417e72359988df077272a7d66ad434
Drake <drakewdong@gmail.com>
2025-01-04 23:30:41 -0500

    update titles

--- graph.py
+++ graph.py
@@ -3,6 +3,7 @@ import sys
 import os
 import matplotlib.pyplot as plt
 
+
 def get_data(file_name):
     folder_path = './weekly_data'
     if not os.path.exists(folder_path):
@@ -21,8 +22,9 @@ def get_data(file_name):
 
     with open(f'./weekly_data/{file_name}', 'r') as file:
         json_data = json.load(file)
-        
+
     return json_data
+
 
 def graph(file_name):
     json_data = get_data(file_name)
@@ -39,21 +41,25 @@ def graph(file_name):
             ticker_count[key] += 1
         else:
             ticker_count[key] = 1
-    
-    sorted_data = sorted(ticker_count.items(), key=lambda x: x[1], reverse=True)
-    labels = [f"{ticker} ({position})" for (ticker, position), count in sorted_data]
+
+    sorted_data = sorted(ticker_count.items(),
+                         key=lambda x: x[1], reverse=True)
+    labels = [f"{ticker} ({position})" for (
+        ticker, position), count in sorted_data]
     counts = [count for _, count in sorted_data]
-    colors = ['red' if position == 'P' else 'blue' for (ticker, position), _ in sorted_data]
+    colors = ['red' if position == 'P' else 'blue' for (
+        ticker, position), _ in sorted_data]
 
     plt.figure(figsize=(10, 6))
     plt.bar(labels, counts, color=colors)
     plt.xlabel("Ticker (Position)", fontsize=10)
     plt.ylabel("Count", fontsize=10)
-    plt.title("Count of Tickers by Position", fontsize=13)
+    plt.title("r/wsb Ticker Frequency by Position", fontsize=13)
     plt.xticks(rotation=90, ha="right", fontsize=5)
     plt.tight_layout()
     plt.show()
-    
+
+
 def graph_frequency(file_name):
     json_data = get_data(file_name)
     ticker_count = {}
@@ -65,29 +71,31 @@ def graph_frequency(file_name):
         if ticker in ticker_count:
             ticker_count[ticker] += 1
         else:
-            ticker_count[ticker] = 1 
-    
-    sorted_data = sorted(ticker_count.items(), key=lambda x: x[1], reverse=True)
-    labels = [ticker for ticker, count in sorted_data]  
+            ticker_count[ticker] = 1
+
+    sorted_data = sorted(ticker_count.items(),
+                         key=lambda x: x[1], reverse=True)
+    labels = [ticker for ticker, count in sorted_data]
     counts = [count for _, count in sorted_data]
-    
-    plt.figure(figsize=(10,6))
+
+    plt.figure(figsize=(10, 6))
     plt.bar(labels, counts)
     plt.xlabel("Ticker", fontsize=10)
     plt.ylabel("Count", fontsize=10)
-    plt.title("Ticker frequency in r/wsb YOLO posts", fontsize=13)
+    plt.title("Ticker Frequency in r/wsb YOLO Posts", fontsize=13)
     plt.xticks(rotation=90, ha="right", fontsize=5)
     plt.tight_layout()
     plt.show()
-    
+
+
 if __name__ == "__main__":
-    
+
     if len(sys.argv) < 3:
         print("Please provide a function and a file name. Format python graph.py function file_name")
     else:
         function_name = sys.argv[1]
         file_name = sys.argv[2]
-        
+
         if function_name == "graph":
             graph(file_name)
         elif function_name == "graph_freq":
